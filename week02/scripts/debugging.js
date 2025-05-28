8927ee6567ab78f3e9be4ad851eb81a606c0bdae
yumahanane <yuma.hanane1@gmail.com>
2025-05-29 01:14:45 +0200

    debugging exercise

---
+++ week02/scripts/debugging.js
@@ -0,0 +1,14 @@
+const radiusOutput = document.getElementById("radius");
+const areaOutput = document.getElementById("area");
+
+let area = 0;
+const PI = 3.14159;
+
+let radius = 10;
+area = PI * radius * radius;
+radiusOutput.textContent = radius;
+
+radius = 20;
+area = PI * radius * radius;
+radiusOutput.textContent = radius;
+areaOutput.textContent = area;
