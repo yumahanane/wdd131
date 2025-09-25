144c3e6193bce184449cfb92576d0741aef8ee6c
yumahanane <yuma.hanane1@gmail.com>
2025-09-26 01:38:17 +0200

    changes added

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -21,3 +21,58 @@ li.appendChild(deleteButton);
 // Append the li element variable to the unordered list in the HTML.
 list.append(li);
 
+// Create a click event listener for the Add Chapter button using an addEventListener.
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        console.log(li);
+    } else {
+        input.focus();
+    }
+});
+
+// Add an event listener to the delete button that removes the li element when clicked.
+deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.focus();
+});
+
+
+
+
+// // Declare three (3) variables that hold references to the input, button, and list elements.
+// const input = document.querySelector('#favchap');
+// const button = document.querySelector('button');
+// const list = document.querySelector('ul');
+
+// // Create a li element that will hold each entry's chapter title and an associated delete button.
+// const li = document.createElement('li');
+
+// // Create a delete button
+// const deleteButton = document.createElement('button');
+
+// // Populate the li element variable's textContent with the input value.
+// li.textContent = input.value;
+
+// // Populate the button textContent with a ❌
+// deleteButton.textContent = '❌';
+
+// // Append the li element variable with the delete button
+// li.appendChild(deleteButton);
+
+// // Append the li element variable to the unordered list in the HTML.
+// list.append(li);
+
+// // Create a click event listener for the Add Chapter button using an addEventListener.
+// button.addEventListener('click', function () {
+//     if (input.value.trim() !== '') {
+//         input.value;
+//     } else {
+//         input.focus();
+//     }
+// });
+
+// // Add an event listener to the delete button that removes the li element when clicked.
+// deleteButton.addEventListener('click', function() {
+//     list.removeChild(li);
+//     input.focus();
+// });
