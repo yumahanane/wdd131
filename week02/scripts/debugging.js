29437dd422dd47cf58096c570e705b91504c5bef
yumahanane <yuma.hanane1@gmail.com>
2025-10-10 18:13:48 +0200

    errors fixed

--- week02/scripts/debugging.js
+++ week02/scripts/debugging.js
@@ -1,15 +1,15 @@
 const radiusOutput = document.getElementById('radius');
-const areaOutput = document.querySelector('area');
+const areaOutput = document.querySelector('#area');
 
 let area = 0;
-const PI == 3.14159;
+const PI = 3.14159;
 
-const radius = 10;
+let radius = 10;
 area = PI * radius * radius;
-radiusOutput = radius;
-areaOutput = area;
+radiusOutput.textContent = radius;
+areaOutput.textContent = area;
 
 radius = 20;
 area = PI * radius * radius;
-radiusOutput = radius;
-areaOutput = area;
+radiusOutput.textContent = radius;
+areaOutput.textContent = area;
