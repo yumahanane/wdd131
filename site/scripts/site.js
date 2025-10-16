9712c6d32907f2e17edb3ae08d7a781a2379d89e
yumahanane <yuma.hanane1@gmail.com>
2025-10-16 11:53:40 +0200

    book page done

--- site/scripts/site.js
+++ site/scripts/site.js
@@ -4,6 +4,41 @@ const today = new Date();
 
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
-
 var date = document.lastModified;
 document.getElementById("lastModified").innerHTML = "Last modification: " + date;
+
+// ------------ Contents ----------- 
+
+const contentsArray = [
+    {
+        chapter: "chap1",
+        title: "Meeting My Psychiatrist"
+    },
+    {
+        chapter: "chap2",
+        title: "A Little Series Taught Me ..."
+    },
+    {
+        chapter: "chap3",
+        title: "For The One I Love"
+    },
+    {
+        chapter: "chap4",
+        title: "One Final Piece of My Mind"
+    },
+]
+
+createContents(contentsArray);
+
+function createContents(indexArray) {
+    document.querySelector("#contents").innerHTML = "";
+    indexArray.forEach(item => {
+        let line = document.createElement("li");
+
+        line.innerHTML = `Chapter - <a href=#${item.chapter}>${item.title}`;
+
+        document.querySelector("#contents").appendChild(line);
+
+    }); 
+}
+
